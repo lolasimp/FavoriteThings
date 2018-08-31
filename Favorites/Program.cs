a0e5be18fdc71dd1550341fec5aff26eca000674
lolasimp <you@example.com>
2018-08-30 20:48:15 -0500

    Add project files.

---
+++ Favorites/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Favorites
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
