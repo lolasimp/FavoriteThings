5bb4468ba53295545d93fdea38fceac03f542753
lolasimp <you@example.com>
2018-09-01 12:12:32 -0500

    foods are printing

--- Favorites/FavThings/Calamari.cs
+++ Favorites/FavThings/Calamari.cs
@@ -6,7 +6,7 @@ namespace Favorites.FavThings
     {
         public Calamari() : base("Calamari", "amazing", "usually never lets me down")
         {
-          //  Console.WriteLine($"Now we come to my 3rd favorite food,");
+
         }
     }
 }
